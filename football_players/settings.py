dbcdf338b4316acdd144451d987b56952657312e
Daniel <daniellima2297@gmail.com>
2020-12-12 20:31:06 -0300

    Updating settings

--- football_players/settings.py
+++ football_players/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-gxy*52(y(^b*(jmxh(_vz!hw005)_&t49k*y7&kiu_^$m867l'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -75,17 +76,13 @@ WSGI_APPLICATION = 'football_players.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #},
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'players',
-        'USER': 'postgres',
-        'PASSWORD': '123456senha',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
 
