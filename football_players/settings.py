bba32b9b1e491c5587167267e6b385d5a1ed8b19
Daniel <daniellima2297@gmail.com>
2020-12-12 20:29:37 -0300

    Updating settings

--- football_players/settings.py
+++ football_players/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'football_players.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #},
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'players',
+        'USER': 'postgres',
+        'PASSWORD': '123456senha',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
